f7139e56f406503638aef0b61a004e75acf223dc
emburyj <joshembury@gmail.com>
2022-12-14 18:56:04 -0800

    added tweepy; added function to import past tweets; added logic to check current table data from web and compare with past tweets; added environ var import;

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from bs4 import BeautifulSoup as BS
 import pathlib
 from urllib.request import Request, urlopen
 import time
+import os
+import tweepy
 
 # import environ vars
-
+bearer_token = os.environ["BEARER_TOKEN"]
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
 
 
@@ -26,6 +32,23 @@ class Data():
     def load_inspection_page(name):
         file_path = pathlib.Path(name)
         return file_path.read_text(encoding='utf8')
+
+    def tweet_check():
+        '''Use tweepy api to retrieve past tweet data
+        input:
+        output: list of tweets(str)
+        '''
+        global bearer_token, consumer_key, consumer_secret, access_token, access_token_secret
+        client = tweepy.Client(bearer_token=bearer_token,
+                       consumer_key=consumer_key,
+                       consumer_secret=consumer_secret,
+                       access_token=access_token,
+                       access_token_secret=access_token_secret)
+        tweets = client.get_home_timeline()
+        past_tweets = []
+        for item in tweets[0]:
+            past_tweets.append(str(item))
+        return past_tweets
 
 class Processor():
     def html_proccessor(html):
@@ -55,25 +78,16 @@ class Processor():
                 table_data.append([row_items[1].text.strip(), row_items[4].text.strip()])
         return table_data
 
+    def parse_temp(soup):
+        temp_tweet = f"\nCurrent temperature is 23.7F"
+        return temp_tweet
 
-    def tweet_check():
-        '''Use tweepy api to retrieve past tweet data
-        input:
-        output: list of tweets(str)
-        '''
-        # global bearer_token, consumer_key, consumer_secret, access_token, access_token_secret
-
-        tweets = client.get_home_timeline()
-        past_tweets = []
-        for item in tweets[0]:
-            past_tweets.append(item)
-        return past_tweets
 
     def groom_tweet_text(groom_name, groom_time):
         # function for generating groom tweet text
         # need to convert UTC to local time
         # include temp
-        tweet_text = f"Grooming of {groom_name} has been completed at {groom_time}"
+        tweet_text = f"{groom_name} has been groomed. Completed at {groom_time}."
         return tweet_text
 
     def status_tweet_text(update):
@@ -88,18 +102,30 @@ class IO():
 # MAIN
 if __name__ == '__main__':
 # get list of tweets
+    past_tweets = Data.tweet_check()
 
     while(True):
         # html = get_inspection_page()
-
-        html = load_inspection_page('inspection_page.html')
-        soup = BS(html)
+        new_tweet_text = []
+        html = Data.load_inspection_page('inspection_page.html')
+        soup = BS(html, features="html5lib")
         current_table_data = Processor.parse_table(soup)
+        temp_tweet = Processor.parse_temp(soup)
         # go through list of current data and check if it has been tweeted
-        for row in current_table_data:
-            current_text = Processor.groom_tweet_text(row[0], row[1])
+        for current in current_table_data:
+            current_text = Processor.groom_tweet_text(current[0], current[1])
+            flag = True
+            for past in past_tweets:
+                # check if current line in table corresponds to a previous tweet
+                if current_text in past:
+                    flag = False
+            if flag:
+                new_tweet_text.append(current_text + temp_tweet) # this indicates it hasn't been tweeted
 
-        time.sleep(60)
+        for text in new_tweet_text:
+            print(text)
+            past_tweets.append(text)
+        time.sleep(5)
 
 
 
